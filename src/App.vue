3052bbffdfa3ff8e117aa9e7c8a5121754e342aa
Malihashahi <maliha.shahi.03@gmail.com>
2025-02-20 06:47:59 +0330

    use vue cli

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <HomeComponet  />
+  </div>
+
+
+
+</template>
+
+<script>
+import HomeComponet from './components/HomeComponet.vue';
+
+
+export default {
+    components:{
+      HomeComponet
+    }
+}
+</script>
+
+<style>
+
+</style>
